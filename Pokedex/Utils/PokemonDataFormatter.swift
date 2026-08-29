45186521c3fa4365a42387734c305283a5b278f3
Izadora Montenegro <izadoraoamontenegro@gmail.com>
2026-08-29 11:15:34 -0300

    refactor: use Measurement in formatter functions

--- Pokedex/Utils/PokemonDataFormatter.swift
+++ Pokedex/Utils/PokemonDataFormatter.swift
@@ -14,16 +14,18 @@ enum PokemonDataFormatter {
             .map { $0.capitalized }
             .joined(separator: " ")
     }
-
+    
     static func number(_ id: Int) -> String {
         String(format: "#%03d", id)
     }
-
+    
     static func height(_ height: Int) -> String {
-        "\(Double(height) / 10)m"
+        Measurement(value: Double(height) / 10, unit: UnitLength.meters)
+            .formatted(.measurement(width: .abbreviated))
     }
-
+    
     static func weight(_ weight: Int) -> String {
-        "\(Double(weight) / 10)kg"
+        Measurement(value: Double(weight) / 10, unit: UnitMass.kilograms)
+            .formatted(.measurement(width: .abbreviated))
     }
 }
